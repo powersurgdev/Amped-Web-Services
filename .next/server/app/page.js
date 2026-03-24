28edaa8567adea2ffc439bac2ab3cd06834a6a35
powersurgdev <sergiossolution@gmail.com>
2026-03-24 13:27:32 +0000

    Stufffffffffffff

--- .next/server/app/page.js
+++ .next/server/app/page.js
@@ -708,7 +708,7 @@ module.exports = require("next/dist/server/app-render/dynamic-access-async-stora
 
 /***/ }),
 
-/***/ "../app-render/work-async-storage.external":
+/***/ "./work-async-storage.external":
 /*!*****************************************************************************!*\
   !*** external "next/dist/server/app-render/work-async-storage.external.js" ***!
   \*****************************************************************************/
@@ -814,7 +814,7 @@ module.exports = require("util");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, ["vendor-chunks/next","vendor-chunks/motion-dom","vendor-chunks/@radix-ui","vendor-chunks/tailwind-merge","vendor-chunks/framer-motion","vendor-chunks/@floating-ui","vendor-chunks/@tanstack","vendor-chunks/lucide-react","vendor-chunks/motion-utils","vendor-chunks/class-variance-authority","vendor-chunks/@swc","vendor-chunks/clsx","vendor-chunks/drizzle-orm","vendor-chunks/react-remove-scroll","vendor-chunks/react-style-singleton","vendor-chunks/react-remove-scroll-bar","vendor-chunks/use-callback-ref","vendor-chunks/@hookform","vendor-chunks/use-sidecar","vendor-chunks/zod","vendor-chunks/tslib","vendor-chunks/react-hook-form","vendor-chunks/drizzle-zod","vendor-chunks/get-nonce","vendor-chunks/aria-hidden"], () => (__webpack_exec__("(rsc)/./node_modules/next/dist/build/webpack/loaders/next-app-loader/index.js?name=app%2Fpage&page=%2Fpage&appPaths=%2Fpage&pagePath=private-next-app-dir%2Fpage.tsx&appDir=%2Fhome%2Frunner%2Fworkspace%2Fapp&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js&rootDir=%2Fhome%2Frunner%2Fworkspace&isDev=true&tsconfigPath=tsconfig.json&basePath=&assetPrefix=&nextConfigOutput=&preferredRegion=&middlewareConfig=e30%3D&isGlobalNotFoundEnabled=!")));
+var __webpack_exports__ = __webpack_require__.X(0, ["vendor-chunks/next","vendor-chunks/motion-dom","vendor-chunks/framer-motion","vendor-chunks/drizzle-orm","vendor-chunks/@radix-ui","vendor-chunks/lucide-react","vendor-chunks/motion-utils","vendor-chunks/@tanstack","vendor-chunks/react-remove-scroll","vendor-chunks/@floating-ui","vendor-chunks/@swc","vendor-chunks/react-style-singleton","vendor-chunks/react-remove-scroll-bar","vendor-chunks/use-callback-ref","vendor-chunks/tailwind-merge","vendor-chunks/clsx","vendor-chunks/@hookform","vendor-chunks/use-sidecar","vendor-chunks/zod","vendor-chunks/tslib","vendor-chunks/react-hook-form","vendor-chunks/drizzle-zod","vendor-chunks/class-variance-authority","vendor-chunks/get-nonce","vendor-chunks/aria-hidden"], () => (__webpack_exec__("(rsc)/./node_modules/next/dist/build/webpack/loaders/next-app-loader/index.js?name=app%2Fpage&page=%2Fpage&appPaths=%2Fpage&pagePath=private-next-app-dir%2Fpage.tsx&appDir=%2Fhome%2Frunner%2Fworkspace%2Fapp&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js&rootDir=%2Fhome%2Frunner%2Fworkspace&isDev=true&tsconfigPath=tsconfig.json&basePath=&assetPrefix=&nextConfigOutput=&preferredRegion=&middlewareConfig=e30%3D&isGlobalNotFoundEnabled=!")));
 module.exports = __webpack_exports__;
 
 })();
